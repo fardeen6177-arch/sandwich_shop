8d98ac5561e7de91edf55d54b447ebe900c573d4
Fardeen Yasin Shaikh <fardeen6177@gmail.com>
2025-12-02 03:15:46 +0530

    Add files via upload

---
+++ hello_dart.dart
@@ -0,0 +1,10 @@
+// Write a function that returns the sum of two integers
+
+int sum(int a, int b) {
+  return a + b;
+}
+
+void main() {
+  int result = sum(10, 20);
+  print("The sum is: $result");
+}
